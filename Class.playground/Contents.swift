e872bb3eb1f8563837dce855c9bdfe23d0612104
TAOyinloye <94188481+TAOyinloye@users.noreply.github.com>
2023-10-08 18:13:20 +0100

    Add files via upload

---
+++ Class.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+class MyCo6225Class  /*define a class
+it requires an initializer
+*/
+{
+    //for the class, we can decalre two variables "fullname of stirng type and "studentID" of integer type
+    var fullName : String
+    var studentId: Int
+    
+    //create an init method.  We need this to create an instance of the class.
+    init(fullName: String, studentId: Int) {
+    self.fullName = fullName
+    self.studentId = studentId
+    }
+}
+var studentOne = MyCo6225Class (fullName: "Toyosi Oyinloye", studentId: 1)
+var studentTwo = studentOne
+studentTwo.fullName = "Andrew Muncey"
+studentTwo.studentId = 2
+print("Fullname of studentOne: \(studentOne.fullName)")
+print("ID of studentOne: \(studentOne.studentId)")
+print("Fullname of studentTwo: \(studentTwo.fullName)")
+print("ID of studentTwo: \(studentTwo.studentId)")
+
+
+
+
+
+//let studentTwo = MyCo6225Class (fullName: "", studentId: 002)
+//let studentThree = MyCo6225Class (fullName: "Lucas Omahony", studentId: 003)
+
